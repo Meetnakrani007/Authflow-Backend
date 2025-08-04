a6171b5d9d880a67fd1c42de9127b40393bc4723
Meet Nakrani <nakranimeet2005@gmail.com>
2025-08-04 20:52:55 +0530

    add identifier

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authController = require("../controllers/authController");
+const { identifier } = require("../middleware/identification");
 const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/signin", authController.signin);
-router.post("/signout", authController.signout);
-router.patch("/sent-code", authController.sendVerificationCode);
-router.patch("/verify-code", authController.verifyVerificationCode);
+router.post("/signout", identifier, authController.signout);
+router.patch("/sent-code", identifier, authController.sendVerificationCode);
+router.patch("/verify-code", identifier, authController.verifyVerificationCode);
 module.exports = router;
